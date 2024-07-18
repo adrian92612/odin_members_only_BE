8ea2c664dcffdc2fd735d31b909d4e27b62e0d0f
adrian92612 <adrian.villamin0612@gmail.com>
2024-07-18 20:29:34 +0800

    added mongoose connect and installed passport jwt

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// connect to MongoDB
+const mongoDB = process.env.DB_URL;
+mongoose.set("strictQuery", false);
+const main = async () => await mongoose.connect(mongoDB);
+main().catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => res.send("TEST"));
+
+app.post("/sign-up", (req, res, next) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
